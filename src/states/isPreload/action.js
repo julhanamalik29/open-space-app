c732053ed8228047be1e0ede9e275f246b56e7cb
Julhan Abdul Malik <julhanabdulmalik@gmail.com>
2025-08-12 22:24:20 +0700

    Membuat Action dan Reducer untuk State isPreload

--- src/states/isPreload/action.js
+++ src/states/isPreload/action.js
@@ -1,3 +1,40 @@
 /**
  * @TODO: Define all the actions (creator) for the isPreLoad state
  */
+
+import api from '../../utils/api';
+import { setAuthUserActionCreator } from '../authUser/action';
+
+const ActionType = {
+  SET_IS_PRELOAD: 'SET_IS_PRELOAD',
+};
+
+function setIsPreloadActionCreator(isPreLoad) {
+  return {
+    type: ActionType.SET_IS_PRELOAD,
+    payload: {
+      isPreLoad,
+    },
+  };
+}
+
+// Function Thunk
+
+function asyncPreloadProcess() {
+  return async (dispatch) => {
+    try {
+      // Preload Process
+      const authUser = await api.getOwnProfile();
+
+      dispatch(setAuthUserActionCreator(authUser));
+    } catch (error) {
+      // Fallback Process
+      dispatch(setAuthUserActionCreator(null));
+    } finally {
+      // End Preload Process
+      dispatch(setIsPreloadActionCreator(false));
+    }
+  };
+}
+
+export { ActionType, setIsPreloadActionCreator, asyncPreloadProcess };
